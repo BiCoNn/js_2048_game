d6181dd63175eb360b1c5b48ab5f354606adeb31
Nazar <nazar.shevchuk.121@gmail.com>
2025-09-07 09:47:36 +0300

    sol1

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -20,12 +20,29 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.state = initialState;
+    this.state1D = this.state.flat();
+    this.startBtn = document.querySelector('.start');
+    this.cells = document.querySelectorAll('.field-cell');
+
+    this.firstClick = true;
+    this.start();
   }
 
-  moveLeft() {}
+  moveLeft() {
+    document.addEventListener('keydown', () => {
+      if()
+    })
+  }
   moveRight() {}
   moveUp() {}
   moveDown() {}
@@ -55,14 +72,75 @@ class Game {
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.startBtn.addEventListener('click', () => {
+      if (this.firstClick) {
+        const [idx1, idx2] = this.getTwoRandomCells();
+
+        this.cells.forEach((cell, i) => {
+          if (i === idx1 || i === idx2) {
+            const val = Math.random() > 0.1 ? 2 : 4;
+
+            cell.textContent = val;
+            cell.className = 'field-cell'; // спершу скидаємо клас
+            cell.classList.add(`field-cell--${val}`);
+          } else {
+            cell.className = 'field-cell';
+          }
+        });
+
+        this.firstClick = false;
+      } else {
+        this.restart();
+      }
+    });
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.state1D = this.state.flat();
+
+    this.cells.forEach((cell) => {
+      cell.textContent = '';
+      cell.className = 'field-cell';
+    });
+
+    this.firstClick = true;
+  }
 
   // Add your own methods here
+  getTwoRandomCells() {
+    const idx1 = Math.floor(Math.random() * this.state1D.length);
+    let idx2;
+
+    do {
+      idx2 = Math.floor(Math.random() * this.state1D.length);
+    } while (idx2 === idx1);
+
+    return [idx1, idx2];
+  }
+
+  move() {
+    document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    
+  }
+
+  if (event.key === 'ArrowRight') {
+    console.log('Натиснуто стрілку вправо');
+  }
+
+  if (event.key === 'ArrowUp') {
+    console.log('Натиснуто стрілку вгору');
+  }
+
+  if (event.key === 'ArrowDown') {
+    console.log('Натиснуто стрілку вниз');
+  }
+});
+  }
 }
 
 module.exports = Game;
